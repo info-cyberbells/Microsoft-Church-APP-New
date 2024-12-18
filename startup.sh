657a9969a75b373fddc3985b9aed66b7d0230109
Cyberbells <info.cyberbells@gmail.com>
2024-12-18 13:16:51 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-gunicorn --bind=0.0.0.0 --timeout 600 application:app
+cd /home/site/wwwroot
+gunicorn --bind=0.0.0.0:8000 --timeout 600 application:app
