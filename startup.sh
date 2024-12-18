f2abc1a6954de3411173ab213c0eabef397ae233
Cyberbells <info.cyberbells@gmail.com>
2024-12-18 16:07:28 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-cd /home/site/wwwroot
-gunicorn --bind=0.0.0.0:8000 --timeout 600 application:app
+
+# Install required system dependencies
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y wget
+
+# Download and install a compatible version of GLIBC
+wget http://ftp.us.debian.org/debian/pool/main/g/glibc/libc6_2.29-1_amd64.deb
+dpkg -i libc6_2.29-1_amd64.deb
+
+# Start Gunicorn
+gunicorn --bind=0.0.0.0:8000 application:app
