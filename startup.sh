6aa0c7a6c22990167c0f50b1f0f2b796fd1a6fc6
Cyberbells <info.cyberbells@gmail.com>
2024-12-19 13:25:18 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
-# Install required system dependencies
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y wget
-
-# Download and install a compatible version of GLIBC
-wget http://ftp.us.debian.org/debian/pool/main/g/glibc/libc6_2.29-1_amd64.deb
-dpkg -i libc6_2.29-1_amd64.deb
-python -m pip install -r requirements.txt
-# Start Gunicorn
+python3 -m pip install -r requirements.txt
 gunicorn --bind=0.0.0.0:8000 application:app
